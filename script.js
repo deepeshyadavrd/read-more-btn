5351410183a347ba96e0fa8b42cef23d36039fae
deepeshyadavrd <raoshsbh6@gmail.com>
2024-07-27 17:56:24 +0530

    animated read more button

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const parentContainer = document.querySelector(".read-more-container");
+const btn = document
+    .querySelector('.read-more-btn');
 
-parentContainer.addEventListener('click', event=>{
-    const current = event.target;
-    const isReadMoreBtn = current.className.includes('read-more-btn');
+const text = document
+    .querySelector('.card__read-more');
 
-    if(!isReadMoreBtn) return;
+const cardHolder = document
+    .querySelector('.card-holder');
 
-    const currentText = event.target.parentNode.querySelector('.read-more-text');
+cardHolder
+    .addEventListener('click', e => {
 
-    currentText.classList.toggle('read-more-text--show');
-    current.textContent = current.textContent.includes('Read More') ? "Read Less.." : "Read More...";
-})
+        const current = e.target;
+
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+
+        if (!isReadMoreBtn)
+            return;
+
+        const currentText = e.target.parentNode.querySelector('.card__read-more');
+
+        currentText.classList.toggle('card__read-more--open');
+
+        current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'Read More...';
+
+    });
