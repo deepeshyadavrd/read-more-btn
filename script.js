2010cea6eaa556605188a7226b8fe3f57e4f8dd2
deepeshyadavrd <raoshsbh6@gmail.com>
2024-07-27 17:10:33 +0530

    Final Commit

--- script.js
+++ script.js
@@ -9,4 +9,5 @@ parentContainer.addEventListener('click', event=>{
     const currentText = event.target.parentNode.querySelector('.read-more-text');
 
     currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ? "Read Less.." : "Read More...";
 })
